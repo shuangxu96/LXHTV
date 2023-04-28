73eaba9e98cb3a38f041392c2f82f4adb145c51f
Shuang Xu <35496697+shuangxu96@users.noreply.github.com>
2023-04-28 13:36:09 +0800

    Update LTHTV_Lq.m

--- LTHTV/LTHTV_Lq.m
+++ LTHTV/LTHTV_Lq.m
@@ -1,37 +1,4 @@
-%% =========================== Frist part notes ===========================
-% ADMM algorithm: tensor denosing
-%    solve the following problem 
-%           (SSTV regularized low rank tucker decomposition problem)
-%    
-%      The approximate model:  
-%                                min tau*||F||_1 + lambda*||E||_1
-%                             s.t. D = X+E, X = Z,DZ = F
-%                                  X = Core*U1*U2*U3,rank(Core_i)=ri 
-%      where,D is SSTV difference operator in the above model
-%
-%      The lagrange function is :
-%       tau*||F||_1 + lambda*||E||_1 + <M1, D-X-E> + <M2,X-Z> +<Gamma,DZ - F>
-%           + beta/2*( ||D-X-E||_F^2 + ||X-Z||_F^2 + ||DZ - F||_F^2 )
-%
-%% =========================== Second part notes===========================
 
-% Reference paper: Hyperspectral Image Restoration via Total Variation 
-%                      Regularized Low-rank Tensor Decomposition
-% Author: Yao Wang, Jiangjun Peng
-% E-mail addresses: andrew.pengjj@gmail.com
-% -------------------------------------------------------------------------
-
-%% =========================== Thrid part notes =========================== 
-% INPUT:
-%   Noi:     noisy 3-D image of size M*N*p normalized to [0,1] band by band
-%   tau:     the trade-off parameter (recommended value 1)              
-%   lambda:  sparse noise coefficient
-%   rank:    rank constraint,[0.8*M,0.8*N,r]
-% OUTPUT:
-%  clean_iamge:   3-D denoised image
-%  S:             The noise term
-%  out_value:     MPSNR and MSSIM and ERGAS valuses of each iteration 
-%  ========================================================================
 function [clean_image,S,out_value,time] = LTNTV(Noi, tau,lambda,rank,beta, shrink_mode)
 tic
 sizeD           = size(Noi);
